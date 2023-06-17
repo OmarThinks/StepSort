58f7af7659492b5dd0eb44b96fa49fde50df54d7
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-17 18:18:56 +0300

    Done testing minMaxLog

--- JsTs/index.test.tsx
+++ JsTs/index.test.tsx
@@ -13,9 +13,25 @@ describe('minMaxLog', () => {
   });
 
   test('zeros', () => {
-    expect(minMaxLog([0,0,0,0,0,0,0,0])).toStrictEqual({ minValue:0, maxValue:0, minLog:0});
+    expect(minMaxLog([0,0,0,0,0,0,0,-0])).toStrictEqual({ minValue:0, maxValue:0, minLog:0});
   });
 
+  test('Normal With Hundreds', () => {
+    expect(minMaxLog([0,100,2,3,4,500,6,7,800,9])).toStrictEqual({ minValue:0, maxValue:800, minLog:0});
+  });
+
+  test('Negatives With Hundreds', () => {
+    expect(minMaxLog([0,-100,-2,-3,-4,-500,-6,-7,-800,-9])).toStrictEqual({ minValue:-800, maxValue:0, minLog:0});
+  });
+
+
+  test('Only Hundreds', () => {
+    expect(minMaxLog([0,100,200,300,400,500,600,700,800,900])).toStrictEqual({ minValue:0, maxValue:900, minLog:2});
+  });
+
+  test('Only Negatives Hundreds', () => {
+    expect(minMaxLog([0,-100,-200,-300,-400,-500,-600,-700,-800,-900])).toStrictEqual({ minValue:-900, maxValue:0, minLog:2});
+  });
 
 
 })
