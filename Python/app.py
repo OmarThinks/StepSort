8cde2af54230af96368ba178162cfaf2041a86fa
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-18 19:12:38 +0300

    get_step_array: tested

--- Python/app.py
+++ Python/app.py
@@ -1,27 +1,49 @@
 from typing import Union, Optional, Tuple, Sequence, List
+
+
 import math
+
+
 from decimal import Decimal
+
 
 Number = Union[int, float]
 
 
 def get_precision(number: Number) -> int:
     """
+
+
     This function gets the precision
+
+
     1,2,3,4,0: 0
+
+
     0.1,-0.1: -1
 
+
+
     100, -100: 2
+
+
     """
+
     if number == 0:
         return 0
+
     number_string = str(number)
+
     if "." in number_string:
         return -len(number_string.split(".")[1])
+
     precision = 0
+
     while number % 10 == 0:
         number = round(number / 10, 10)
+
         precision += 1
+
     return precision
 
 
@@ -29,15 +51,28 @@ def get_min_max_log(
     numbers: List[Number], step: Optional[Number] = None, accuracy: Optional[int] = 12
 ) -> Tuple[Number, Number, int]:
     """
+
+
     This function returns an object list of 3 things:
+
+
     1. Min value in the list
+
+
     2. Max value in the list
+
+
     3. Min log in the list (Rounded down to integer)
+
+
     """
 
     minValue: Number = math.inf
+
     maxValue: Number = -math.inf
+
     minLog: float = math.inf
+
     currentLog = None
 
     minValue = min(numbers)
@@ -49,15 +84,20 @@ def get_min_max_log(
         if not step:
             if number == 0:
                 continue
+
             if number == minValue:
                 continue
+
             currentLog = get_precision(round(number - minValue, 12))
+
             # print("currentLog", currentLog, number, minValue)
+
             if currentLog < minLog:
                 minLog = currentLog
 
     if minLog == math.inf:
         minLog = 0
+
     minLog = int(minLog)
 
     return (minValue, maxValue, int(minLog))
@@ -71,7 +111,7 @@ def get_value_from_storage_index(index: Number, minValue: Number, step: Number):
     return index * step + minValue
 
 
-def get_step_array(
+def get_step_array_full(
     numbers: List[Number], step: Optional[Number] = None, accuracy: Optional[int] = 12
 ) -> List[int]:
     if len(numbers) == 0:
@@ -90,6 +130,7 @@ def get_step_array(
         step = math.pow(10, get_precision(minValue))
 
     # print(minValue, maxValue, minLog)
+
     # print(step)
 
     array_length = int(round(1 + ((maxValue - minValue) / step), 10))
@@ -111,13 +152,14 @@ def get_step_array(
     return sortedIndexes
 
 
-def get_step_array2(
+def get_step_array(
     numbers: List[Number],
     minValue: Number,
     maxValue: Number,
     step: Number,
 ) -> List[int]:
     array_length = int(round(1 + ((maxValue - minValue) / step), 10))
+
     sortedIndexes: List[int] = [0 for x in range(array_length)]
     for number in numbers:
         storageIndex = get_storage_index(number, minValue, step)
--- Python/test_app.py
+++ Python/test_app.py
@@ -1,40 +1,56 @@
 """
+
+
 Testing the sorting algorithm
+
+
 """
 
+
 import unittest
+
 
 from app import (
     get_precision,
     get_min_max_log,
     get_storage_index,
     get_value_from_storage_index,
+    get_step_array_full,
     get_step_array,
-    get_step_array2,
 )
 
 
 class TestGetPrecision(unittest.TestCase):
     def test_get_precision(self):
         self.assertEqual(get_precision(0), 0)
+
         self.assertEqual(get_precision(-1), 0)
+
         self.assertEqual(get_precision(1), 0)
+
         self.assertEqual(get_precision(1000), 3)
+
         self.assertEqual(get_precision(-1000), 3)
+
         self.assertEqual(get_precision(1.43), -2)
+
         self.assertEqual(get_precision(-1.43), -2)
 
 
 class TestGetMinMaxLog(unittest.TestCase):
     def test_get_min_max_log(self):
         self.assertEqual(get_min_max_log([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), (0, 9, 0))
+
         self.assertEqual(
             get_min_max_log([0, -1, -2, -3, -4, -5, -6, -7, -8, -9]), (-9, 0, 0)
         )
+
         self.assertEqual(get_min_max_log([0, 0, 0, 0, 0, 0, 0, 0]), (0, 0, 0))
+
         self.assertEqual(
             get_min_max_log([0, 100, 2, 3, 4, 500, 6, 7, 800, 9]), (0, 800, 0)
         )
+
         self.assertEqual(
             get_min_max_log([0, -100, -2, -3, -4, -500, -6, -7, -800, -9]),
             (
@@ -43,48 +59,88 @@ class TestGetMinMaxLog(unittest.TestCase):
                 0,
             ),
         )
+
         self.assertEqual(
             get_min_max_log([0, 100, 200, 300, 400, 500, 600, 700, 800, 900]),
             (0, 900, 2),
         )
+
         self.assertEqual(
             get_min_max_log([0, -100, -200, -300, -400, -500, -600, -700, -800, -900]),
             (-900, 0, 2),
         )
+
         self.assertEqual(get_min_max_log([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 1), (0, 9, 0))
+
         self.assertEqual(get_min_max_log([0, 0, 0.1, 0.2, 0.9], 0.1), (0, 0.9, 0))
 
 
 class TestGetStorageIndex(unittest.TestCase):
     def test_get_storage_index(self):
         self.assertEqual(get_storage_index(0, 0, 1), 0)
+
         self.assertEqual(get_storage_index(5, 0, 1), 5)
+
         self.assertEqual(get_storage_index(5, 1, 1), 4)
+
         self.assertEqual(get_storage_index(-5, -10, 1), 5)
+
         self.assertEqual(get_storage_index(1, 0, 0.1), 10)
+
         self.assertEqual(get_storage_index(4.5, 0, 1), 4)
 
 
 class TestGetValueFromStorageIndex(unittest.TestCase):
     def test_get_value_from_storage_index(self):
         self.assertEqual(get_value_from_storage_index(0, 0, 1), 0)
+
         self.assertEqual(get_value_from_storage_index(5, 0, 1), 5)
+
         self.assertEqual(get_value_from_storage_index(5, 1, 1), 6)
+
         self.assertEqual(get_value_from_storage_index(5, -10, 1), -5)
+
         self.assertEqual(get_value_from_storage_index(4, 5, 0.1), 5.4)
+
         self.assertEqual(get_value_from_storage_index(5, 0.1, 1), 5.1)
+
+
+class TestGetStepArrayFull(unittest.TestCase):
+    def test_get_step_array_full(self):
+        self.assertEqual(get_step_array_full([0]), [1])
+
+        self.assertEqual(get_step_array_full([0, 0, 0, 0]), [4])
+
+        self.assertEqual(get_step_array_full([1, 2, 3, 4, 5]), [1, 1, 1, 1, 1])
+
+        self.assertEqual(get_step_array_full([1, 1, 2, 2]), [2, 2])
+
+        self.assertEqual(get_step_array_full([1, 1, 0, 3, 3]), [1, 2, 0, 2])
+
+        self.assertEqual(
+            get_step_array_full([-4, -4, 2, 5]), [2, 0, 0, 0, 0, 0, 1, 0, 0, 1]
+        )
+
+        self.assertEqual(get_step_array_full([1.1, 1.4]), [1, 0, 0, 1])
 
 
 class TestGetStepArray(unittest.TestCase):
     def test_get_step_array(self):
-        self.assertEqual(get_step_array([0]), [1])
-        self.assertEqual(get_step_array([0, 0, 0, 0]), [4])
-        self.assertEqual(get_step_array([1, 2, 3, 4, 5]), [1, 1, 1, 1, 1])
-        self.assertEqual(get_step_array([1, 1, 2, 2]), [2, 2])
-        self.assertEqual(get_step_array([1, 1, 0, 3, 3]), [1, 2, 0, 2])
-        self.assertEqual(get_step_array([-4, -4, 2, 5]), [2, 0, 0, 0, 0, 0, 1, 0, 0, 1])
+        self.assertEqual(get_step_array([0], 0, 0, 1), [1])
 
-        self.assertEqual(get_step_array([1.1, 1.4]), [1, 0, 0, 1])
+        self.assertEqual(get_step_array([0, 0, 0, 0], 0, 0, 1), [4])
+
+        self.assertEqual(get_step_array([1, 2, 3, 4, 5], 1, 5, 1), [1, 1, 1, 1, 1])
+
+        self.assertEqual(get_step_array([1, 1, 2, 2], 1, 2, 1), [2, 2])
+
+        self.assertEqual(get_step_array([1, 1, 0, 3, 3], 0, 3, 1), [1, 2, 0, 2])
+
+        self.assertEqual(
+            get_step_array([-4, -4, 2, 5], -4, 5, 1), [2, 0, 0, 0, 0, 0, 1, 0, 0, 1]
+        )
+
+        self.assertEqual(get_step_array([1.1, 1.4], 1.1, 1.4, 0.1), [1, 0, 0, 1])
 
 
 if __name__ == "__main__":
