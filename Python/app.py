2d4c8fbb11f566279f377444318139301b124a24
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-18 06:42:56 +0300

    craeted app.test

---
+++ Python/app.py
@@ -0,0 +1,20 @@
+def get_precision(number):
+    """
+    This function gets the precision
+    1,2,3,4,0: 0
+    0.1,-0.1: -1
+    100, -100: 2
+    """
+    if number == 0:
+        return 0
+
+    number_string = str(number)
+    if "." in number_string:
+        return -len(number_string.split(".")[1])
+
+    precision = 0
+    while number % 10 == 0:
+        number /= 10
+        precision += 1
+
+    return precision
